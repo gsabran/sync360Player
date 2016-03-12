7bdba5714b62278e9b39bb2fa384f1f56db1489b
Saeid Farivar <saeid.farivar@gmail.com>
2016-03-12 12:43:50 -0800

    added a echo websocket server.

--- meteor/server/websocket.js
+++ meteor/server/websocket.js
@@ -1,2 +1,19 @@
-import WebSocket from 'ws';
-console.log('test');
+import WS from 'ws';
+
+var WebSocketServer = require('ws').Server,
+	wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  	console.log("ws: " + getClientIPAddress(ws));
+  	ws.on('message', function incoming(message) {
+    	console.log('received: %s', message);
+
+    	ws.send(message);
+  	});
+});
+
+console.log("websocket listening on port 8080");
+
+function getClientIPAddress(socket) {
+	return socket.upgradeReq.headers['x-forwarded-for'] || socket.upgradeReq.connection.remoteAddress
+}
