026cbdbd79634bf5431e711d9e7734203b66cfed
gsabran <sabranguillaume@gmail.com>
2016-03-13 11:56:26 -0700

    get it ro work

--- meteor/client/lib/positionHelper.js
+++ meteor/client/lib/positionHelper.js
@@ -31,6 +31,7 @@ export const interpolate = (key, position=null) => {
     if (!lastPosition.target) {
       return null;
     }
+    // return lastPosition.target.position;
     if (!lastPosition.lastPositionSend) {
       // first pass: return the given position, don't interpolate
       lastPosition.lastPositionSend = {
@@ -70,3 +71,129 @@ export const getPointsDistance = (a, b, R=1) => {
 
   return R * c;
 };
+
+
+// give the angle corresponding to @rot between @min and @max
+const inBetween = (rot, min, max) => {
+  while (rot > max) {
+    rot -= max - min;
+  }
+  while (rot < min) {
+    rot += max - min;
+  }
+  return rot;
+}
+
+
+// show object that are not in the view in the sides
+export const getCapedRotation = (_cameraRotation, _rot, vfov, hfov) => {
+  if (!_cameraRotation) {
+    console.log('no rotation!')
+  }
+  if (!_cameraRotation)
+    return null;
+
+  // copy data to avoid errors
+  const cameraRotation = {x: _cameraRotation.x, y: _cameraRotation.y};
+  const rot = {x: _rot.x, y: _rot.y};
+
+  const topLeft = {
+    x: cameraRotation.x + vfov / 2,
+    y: cameraRotation.y + hfov / 2,
+    z: 0,
+  };
+
+  const topRight = {
+    x: cameraRotation.x + vfov / 2,
+    y: cameraRotation.y - hfov / 2,
+    z: 0,
+  };
+
+  const bottomLeft = {
+    x: cameraRotation.x - vfov / 2,
+    y: cameraRotation.y + hfov / 2,
+    z: 0,
+  };
+
+  const bottomRight = {
+    x: cameraRotation.x - vfov / 2,
+    y: cameraRotation.y - hfov / 2,
+    z: 0,
+  };
+
+
+  cameraRotation.x = inBetween(cameraRotation.x, -90, 90);
+  cameraRotation.y = inBetween(cameraRotation.y, 0, 360);
+
+  rot.x = inBetween(rot.x, -90, 90);
+  rot.y = inBetween(rot.y, 0, 360);
+  if (rot.x < cameraRotation.x + vfov / 2 && rot.x > cameraRotation.x - vfov / 2 && (
+    (rot.y < cameraRotation.y + hfov / 2 && rot.y > cameraRotation.y - hfov / 2) ||
+    (rot.y - 360 < cameraRotation.y + hfov / 2 && rot.y - 360 > cameraRotation.y - hfov / 2) ||
+    (rot.y + 360 < cameraRotation.y + hfov / 2 && rot.y + 360 > cameraRotation.y - hfov / 2)
+  )) {
+    // the the object is in the view
+    return rot
+  }
+
+  let side = 0;
+  let high = 0;
+
+  if (rot.x > cameraRotation.x + vfov / 2) {
+    // too high
+    high = 1;
+
+  } else if (rot.x < cameraRotation.x - vfov / 2) {
+    // too low
+    high = -1;
+
+  }
+
+  if ((rot.y > cameraRotation.y + hfov / 2 && rot.y < cameraRotation.y + 180) ||
+    (rot.y > cameraRotation.y + hfov / 2 + 360 && rot.y < cameraRotation.y + 180 + 360) ||
+    (rot.y > cameraRotation.y + hfov / 2 - 360 && rot.y < cameraRotation.y + 180 - 360)) {
+    // too on the left
+    side = 1;
+
+  } else if ((rot.y < cameraRotation.y - hfov / 2 && rot.y > cameraRotation.y - 180) ||
+    (rot.y < cameraRotation.y - hfov / 2 + 360 && rot.y > cameraRotation.y - 180 + 360) ||
+    (rot.y < cameraRotation.y - hfov / 2 - 360 && rot.y > cameraRotation.y - 180 - 360)) {
+    // too on the right
+    side = -1;
+
+  }
+  if (high && side) {
+    // closest is a corner
+    if (high === 1 && side === 1) {
+      return topLeft;
+    } else if (high === 1 && side === -1) {
+      return topRight;
+    } else if (high === -1 && side === 1) {
+      return bottomLeft;
+    } else {
+      return bottomRight;
+    }
+  } else {
+    if (high === 1) {
+      return {
+        x: cameraRotation.x + vfov / 2,
+        y: rot.y,
+      }
+    } else if (high === -1) {
+      return {
+        x: cameraRotation.x - vfov / 2,
+        y: rot.y,
+      }
+    } else if (side === 1) {
+      return {
+        x: rot.x,
+        y: cameraRotation.y + hfov / 2,
+      }
+    } else if (side === -1) {
+      return {
+        x: rot.x,
+        y: cameraRotation.y - hfov / 2,
+      }
+    }
+  }
+}
--- meteor/client/modules/VideoPlayer.jsx
+++ meteor/client/modules/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 import React, {PropTypes, Component} from 'react';
 import aframe from 'aframe';
 import ReactDOM from 'react-dom';
-import {angleToPosition, interpolate, getPointsDistance} from '../lib/positionHelper.js';
+import {angleToPosition, interpolate, getPointsDistance, getCapedRotation} from '../lib/positionHelper.js';
 import {Socket} from '../lib/socketManager.js';
 
 
@@ -24,6 +24,8 @@ export default class VideoPlayer extends Component {
     // no change
     if (this.socket && this.rotation && this.rotation.x === rotation.x && this.rotation.y === rotation.y)
       return;
+
+    this.setState({rotation});
 
     if (this.socket) {
       this.socket.emit('setRotation', rotation);
@@ -65,8 +67,8 @@ export default class VideoPlayer extends Component {
 
   render () {
     var rotationsData = this.state;
-    const vfov = 80;
-    const aspect = 1094.0 / 1402;//1.0 * window.innerHeight / window.innerWidth;
+    const vfov = 80 - 10; // -10 to keep things visible and not just on the border
+    const aspect = 1.0 * window.innerWidth / window.innerHeight;
     const hfov = 2 * Math.atan( Math.tan( vfov * Math.PI / 180 / 2 ) * aspect ) * 180 / Math.PI;
 
     // convert position to string
@@ -74,148 +76,7 @@ export default class VideoPlayer extends Component {
       return position.x + " " + position.y + " " + position.z;
     }
 
-    const getBorder = () => {
-
-      const rotation = this.rotation;
-      if (!rotation)
-        return null;
-
-      // give the angle corresponding to @rot between @min and @max
-      const inBetween = (rot, min, max) => {
-        while (rot > max) {
-          rot -= max - min;
-        }
-        while (rot < min) {
-          rot += max - min;
-        }
-        return rot;
-      }
-
-      rotation.x = inBetween(rotation.x, -90, 90);
-      rotation.y = inBetween(rotation.y, 0, 360);
-
-      const topLeft = {
-        x: rotation.x + vfov / 2,
-        y: rotation.y + hfov / 2,
-        z: 0,
-      };
-
-      const topRight = {
-        x: rotation.x + vfov / 2,
-        y: rotation.y - hfov / 2,
-        z: 0,
-      };
-
-      const bottomLeft = {
-        x: rotation.x - vfov / 2,
-        y: rotation.y + hfov / 2,
-        z: 0,
-      };
-
-      const bottomRight = {
-        x: rotation.x - vfov / 2,
-        y: rotation.y - hfov / 2,
-        z: 0,
-      };
-
-      const center = {
-        x: rotation.x,
-        y: rotation.y,
-        z: 0,
-      };
-
-      // show object that are not in the view in the sides
-      const getCapedRotation = (rot) => {
-        rot.x = inBetween(rot.x, -90, 90);
-        rot.y = inBetween(rot.x, 0, 360);
-        console.log(vfov, hfov, rot.y - rotation.y, rot.x - rotation.x);
-        if (rot.x < rotation.x + vfov / 2 && rot.x > rotation.x - vfov / 2 && (
-          (rot.y < rotation.y + hfov / 2 && rot.y > rotation.y - hfov / 2) ||
-          (rot.y - 360 < rotation.y + hfov / 2 && rot.y - 360 > rotation.y - hfov / 2) ||
-          (rot.y + 360 < rotation.y + hfov / 2 && rot.y + 360 > rotation.y - hfov / 2)
-        )) {
-          // the the object is in the view
-          console.log('in screen');
-          return rot
-        }
-
-        let side = 0;
-        let high = 0;
-
-        if (rot.x > rotation.x + vfov / 2) {
-          // too high
-          console.log('too high');
-          high = 1;
-
-        } else if (rot.x < rotation.x - vfov / 2) {
-          // too low
-          console.log('too low');
-          high = -1;
-
-        }
-
-        if ((rot.y > rotation.y + hfov / 2 && rot.y < rotation.y + 180) ||
-          (rot.y > rotation.y + hfov / 2 + 360 && rot.y < rotation.y + 180 + 360) ||
-          (rot.y > rotation.y + hfov / 2 - 360 && rot.y < rotation.y + 180 - 360)) {
-          // too on the left
-          console.log('too on the left');
-          side = -1;
-
-        } else if ((rot.y < rotation.y - hfov / 2 && rot.y > rotation.y - 180) ||
-          (rot.y < rotation.y - hfov / 2 + 360 && rot.y > rotation.y - 180 + 360) ||
-          (rot.y < rotation.y - hfov / 2 - 360 && rot.y > rotation.y - 180 - 360)) {
-          // too on the right
-          console.log('too on the right');
-          side = 1;
-
-        }
-        if (high && side) {
-          // closest is a corner
-          if (high === 1 && side === 1) {
-            return topLeft;
-          } else if (high === 1 && side === -1) {
-            return topRight;
-          } else if (high === -1 && side === 1) {
-            return bottomLeft;
-          } else {
-            return bottomRight;
-          }
-        } else {
-          if (high === 1) {
-            return {
-              x: rotation.x + vfov / 2,
-              y: rot.y,
-            }
-          } else if (high === -1) {
-            return {
-              x: rotation.x - vfov / 2,
-              y: rot.y,
-            }
-          } else if (side === 1) {
-            return {
-              x: rot.x,
-              y: rotation.y + hfov / 2,
-            }
-          } else if (side === -1) {
-            return {
-              x: rot.x,
-              y: rotation.y - hfov / 2,
-            }
-          }
-          console.log('!!!! error!');
-        }
-      }
-      // getCapedRotation({x: 0, y: 0});
-
-      return [
-        <a-cube position={getPos(angleToPosition(5, topLeft))} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={0}></a-cube>,
-        <a-cube position={getPos(angleToPosition(5, topRight))} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={1}></a-cube>,
-        <a-cube position={getPos(angleToPosition(5, bottomLeft))} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={2}></a-cube>,
-        <a-cube position={getPos(angleToPosition(5, bottomRight))} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={3}></a-cube>,
-        <a-cube position={getPos(angleToPosition(5, center))} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={4}></a-cube>,
-        <a-cube position={getPos(angleToPosition(5, getCapedRotation({x: 0, y: 0})))} rotation="30 30 0" width="1" depth="1" height="1" color="green" roughness="0.8" key={5}></a-cube>,
-      ]
-    }
+    const cameraRotation = this.rotation;
 
     return <div>
       <a-scene stats="true">
@@ -229,14 +90,18 @@ export default class VideoPlayer extends Component {
                       segmentsHeight: 64;" material="shader: flat; src: #video" scale="-1 1 1">
         </a-entity>    
         {Object.keys(rotationsData).map(function(userId) {
+          if (userId === 'rotation')
+            return;
+
           const rotationData = rotationsData[userId];
           if (!rotationData)
             return null;
 
-          const position = angleToPosition(5, rotationData);
+          const newRotation = getCapedRotation(cameraRotation, rotationData, vfov, hfov) || rotationData;
+          const distance = getPointsDistance(newRotation, rotationData);
+          const position = angleToPosition(5 * (1 + distance), newRotation);
           return <a-cube position={getPos(position)} rotation="30 30 0" width="1" depth="1" height="1" color="#F16745" roughness="0.8" key={userId}></a-cube>
         })}
-        {getBorder()}
     </a-scene>
     </div>
   }
