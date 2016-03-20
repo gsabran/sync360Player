2ec31c450adca917e7164231fc70119a90a693d8
gsabran <sabranguillaume@gmail.com>
2016-03-20 15:46:21 -0700

    hardcode route to make it work

--- meteor/client/lib/socketManager.js
+++ meteor/client/lib/socketManager.js
@@ -2,16 +2,16 @@ export const Socket = {
   // create a new channel and initiate the socket if necessary, and execute cb when possible
   create: function(rotationServerIPAddress, rotationServerPort, cb) {
     if (typeof io !== 'undefined')
-      return cb(io('http://' + rotationServerIPAddress + ':' + rotationServerPort));
+      return cb(io('http://localhost:3002'));
 
     $.getScript('https://cdn.socket.io/socket.io-1.4.3.js', function() {
-      cb(io('http://' + rotationServerIPAddress + ':' + rotationServerPort));
+      cb(io('http://localhost:3002'));
     });
   },
   getRotationServerPort: function() {
   	return '3002';
   },
   getRotationServerIPAddress: function() {
-  	return '192.168.43.12';
+  	return 'localhost/';
   }
 };
